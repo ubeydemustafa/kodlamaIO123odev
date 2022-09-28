226ed2792232f117c2d05168f9425591c76a268b
ubeydemustafa <mustafaubeydeezici@gmail.com>
2022-09-28 17:42:04 +0300

    10.videoya kadar örnekler yapıldı.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("merhaba zalim dünya");
+        String text = "Erciyes üniversitesinden selamlar ";
+        int x = 10;
+        double y = 11.1;
+        char karakter = 'a';
+        boolean dogruMu = false;
+        System.out.println(text);
+        System.out.println(text+x);
+        System.out.println(text+x);
+        System.out.println(text+x);
+        System.out.println(text+x);
+        System.out.println(text+x);
+        if(dogruMu == true){
+            System.out.println(karakter);
+        } else if (x < y) {
+            System.out.println("y büyük abi");
+        } else{
+            System.out.println("bir tuhaflık var");
+        }
+        int sayi1 = 24;
+        int sayi2 = 25;
+        int sayi3 = 18;
+
+        int enBuyukSayi = sayi1;
+
+        if(enBuyukSayi<sayi2){
+            enBuyukSayi = sayi2;
+        }
+        if (enBuyukSayi<sayi3){
+            enBuyukSayi = sayi3;
+        }
+        System.out.println("en büyük sayı: "+enBuyukSayi);
+
+
     }
 }
